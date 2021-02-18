3354e77d70f13f684fc96ead47d70ce2a7df44e3
manneharikrishna <34763583+manneharikrishna@users.noreply.github.com>
2021-02-19 01:56:26 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+apt-get update && apt-get install awscli -y
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 aws configure set default.region ${region}
