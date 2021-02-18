1572046ad846a5e1d9dc799a5ae3ab8d2b779b0a
manneharikrishna <34763583+manneharikrishna@users.noreply.github.com>
2021-02-19 01:23:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
-/usr/local/bin/aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
-/usr/local/bin/aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-/usr/local/bin/aws configure set default.region ${region}
+#!/bin/bash
+set -e
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+aws configure set default.region ${region}
 
-/usr/local/bin/aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin 556789283657.dkr.ecr.ap-southeast-1.amazonaws.com
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin 556789283657.dkr.ecr.ap-southeast-1.amazonaws.com
 
 docker push 556789283657.dkr.ecr.ap-southeast-1.amazonaws.com/hari:latest
 
-/usr/local/bin/aws ecs register-task-definition --family ${taskdef} --cli-input-json file://taskdef.json --region ${region} --profile default
+aws ecs register-task-definition --family ${taskdef} --cli-input-json file://taskdef.json --region ${region} --profile default
 
-/usr/local/bin/aws --region ${region} ecs update-service --cluster ${cluster_name} --service ${service_name} --task-definition ${task_def_arn} --profile default --desired-count 1
+aws --region ${region} ecs update-service --cluster ${cluster_name} --service ${service_name} --task-definition ${task_def_arn} --profile default --desired-count 1
